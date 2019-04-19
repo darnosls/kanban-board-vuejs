b284a868e5058233260edd8894f82caae1e6f758
darnos <darnoslima@gmail.com>
2019-04-18 21:30:29 -0300

    create action createTask

--- src/store.js
+++ src/store.js
@@ -5,12 +5,27 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    task: {
+      name: '',
+      description: '',
+      step: 'backlog'
+    }
+  },
+  getters: {
+    task(state) {
+      return state.task
+    }
   },
   mutations: {
-
+    SET_TASK: function(state, payload) {
+      state.task = payload
+    }
   },
   actions: {
-
+    async createTask({ commit }, { payload }) {
+      console.log('Criando tarefas')
+      commit('SET_TASK', payload)
+      return true
+    }
   }
 })
